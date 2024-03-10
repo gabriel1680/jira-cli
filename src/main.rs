dfd2b3ea3fa9f57584a415b372967ab93a800cf4
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2024-03-10 16:18:04 -0300

    feat: add ui console

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod dao;
 mod json_file_database_adapter;
 mod models;
+mod ui;
 
 fn main() {
     println!("WIP");
--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,20 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt::Display};
 
 use serde::{Deserialize, Serialize};
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Action {
+    NavigateToEpicDetail { epic_id: u32 },
+    NavigateToStoryDetail { epic_id: u32, story_id: u32 },
+    NavigateToPreviousPage,
+    CreateEpic,
+    UpdateEpicStatus { epic_id: u32 },
+    DeleteEpic { epic_id: u32 },
+    CreateStory { epic_id: u32 },
+    UpdateStoryStatus { story_id: u32 },
+    DeleteStory { epic_id: u32, story_id: u32 },
+    Exit,
+}
 
 #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
 pub enum Status {
@@ -8,6 +22,25 @@ pub enum Status {
     InProgress,
     Closed,
     Resolved,
+}
+
+impl Display for Status {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Open => {
+                write!(f, "OPEN")
+            }
+            Self::InProgress => {
+                write!(f, "IN PROGRESS")
+            }
+            Self::Resolved => {
+                write!(f, "RESOLVED")
+            }
+            Self::Closed => {
+                write!(f, "CLOSED")
+            }
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
