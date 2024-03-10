32110625b7ca5c98acf6e19234804ed9dfd55478
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2024-03-10 14:31:42 -0300

    feat: add jira dao

--- src/models.rs
+++ src/models.rs
@@ -1,30 +1,35 @@
 use std::collections::HashMap;
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
 pub enum Status {
     Open,
     InProgress,
     Closed,
-    Resolved
+    Resolved,
 }
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
 pub struct Epic {
     pub name: String,
     pub description: String,
     pub status: Status,
-    pub stories: Vec<u32>
+    pub stories: Vec<u32>,
 }
 
 impl Epic {
     pub fn new(name: String, description: String) -> Self {
-       Self { name, description, status: Status::Open, stories: vec![] }
+        Self {
+            name,
+            description,
+            status: Status::Open,
+            stories: vec![],
+        }
     }
 }
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
 pub struct Story {
     pub name: String,
     pub description: String,
@@ -33,14 +38,17 @@ pub struct Story {
 
 impl Story {
     pub fn new(name: String, description: String) -> Self {
-        Self { name, description, status: Status::Open }
+        Self {
+            name,
+            description,
+            status: Status::Open,
+        }
     }
 }
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
 pub struct DBState {
     pub last_item_id: u32,
     pub epics: HashMap<u32, Epic>,
     pub stories: HashMap<u32, Story>,
 }
-
